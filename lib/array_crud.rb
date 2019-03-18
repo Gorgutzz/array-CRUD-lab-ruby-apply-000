d70d6339b6c7b9aa95b45124652daf5eed83f60c
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-18 18:00:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   fruits = ["apples", "oranges", "berries", "melons"]
-  fruits << "avacado"
+  fruits << "bananna"
 end
 
 def add_element_to_start_of_array(array, element)
