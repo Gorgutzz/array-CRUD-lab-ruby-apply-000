2ca7ed6d6066e69af034cb482c6710a8d806a63b
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-18 17:59:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fruits = [apples, oranges, berries, melons]
+  fruits = ["apples", "oranges", "berries", "melons"]
 end
 
 def add_element_to_end_of_array(array, element)
